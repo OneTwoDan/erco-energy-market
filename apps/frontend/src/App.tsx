5cadb4aaca4aa829dcadf0a4e206120eb3746061
OneTwoDan <danielondonoa@gmail.com>
2025-05-29 11:33:21 -0500

    Set up routing with react-router-dom

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,7 +1,16 @@
-import { DashboardLayout } from '@/components/layout/DashboardLayout';
+import { Routes, Route } from 'react-router-dom';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Home from './pages/Home';
 
 function App() {
-  return <DashboardLayout />;
+  return (
+    <Routes>
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/dashboard" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ apps/frontend/src/pages/Home.tsx
@@ -0,0 +1,7 @@
+import { DashboardLayout } from '@/components/layout/DashboardLayout';
+
+const Home = () => {
+  return <DashboardLayout />;
+};
+
+export default Home;
